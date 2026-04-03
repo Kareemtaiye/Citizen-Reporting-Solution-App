c706f8737c68819c7668b9532f348b42efdf3fc3
Kareem Taiye <olayinkahassan117@gmail.com>
2026-04-03 11:23:49 +0100

    cors fix

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ import reportRouter from "./routes/reportRoutes.js";
 import globalErrorHandler from "./middlewares/globalErrHandler.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
